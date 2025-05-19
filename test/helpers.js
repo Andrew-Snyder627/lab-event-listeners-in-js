8e26759e29fb92608c10d6077bd330abdd34d0d3
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-05-19 19:13:01 -0400

    Saving work for final submission

--- test/helpers.js
+++ test/helpers.js
@@ -14,13 +14,13 @@
 // }
 
 
-const { JSDOM } = require('jsdom')
-const fs = require('fs')
-const path = require('path')
+const { JSDOM } = require('jsdom');
+const fs = require('fs');
+const path = require('path');
 
-const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8')
-const dom = new JSDOM(html)
+const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+const dom = new JSDOM(html);
 
-global.document = dom.window.document
-global.window = dom.window
-module.exports = { document }
+global.document = dom.window.document;
+global.window = dom.window;
+module.exports = { document };
